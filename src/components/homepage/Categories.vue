734cc3707e233706f6654cd54b5fbaefab6e5910
dkfauzzi <dkfauzzi@gmail.com>
2024-10-18 19:19:27 +0700

    Dynamic Item Cards

---
+++ src/components/homepage/Categories.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import CategoriesCard from './../CategoriesCard.vue';
+
+const categories = ref([
+  { id: 1, title: 'Mobile UI Kit', image: 'categories-1.jpg', count: 731 },
+  { id: 2, title: 'Fonts', image: 'categories-2.jpg', count: 657 },
+  { id: 3, title: 'Icon Set', image: 'categories-3.jpg', count: 83559 },
+  { id: 4, title: 'Website UI Kit', image: 'categories-4.jpg', count: 4500 },
+
+])
+</script>
+<template>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <CategoriesCard v-for="categories in categories" :key="categories.id" :title="categories.title"
+        :image="categories.image" :count="categories.count" />
+
+
+    </div>
+  </div>
+</template>
---
+++ src/components/homepage/Items.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import ItemsCard from '../ItemsCard.vue';
+
+const items = ref([
+    {id: 1, title:'RoboCrypto App', subTitle:'Mobile UI Kit', image:'items-1.jpg'},
+    {id: 2, title:'Online Doctor Consultation', subTitle:'Website UI Kit', image:'items-2.jpg'},
+    {id: 3, title:'Banking Crypto', subTitle:'Mobile UI Kit', image:'items-3.jpg'},
+])
+</script>
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+      <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+      <div class="flex flex-wrap -mx-1 lg:-mx-4">
+        <ItemsCard v-for="items in items" :key="items.id" :title="items.title"
+        :subTitle="items.subTitle" :image="items.image" /> 
+        
+
+      </div>
+    </div>
+
+
+</template>
