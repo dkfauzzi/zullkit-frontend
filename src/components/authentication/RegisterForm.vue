c82750efab0d395817c08534d229ba963884e25a
dkfauzzi <dkfauzzi@gmail.com>
2024-10-22 22:27:11 +0700

    Login, Register and User Info with Pinia-axios

--- src/components/authentication/RegisterForm.vue
+++ src/components/authentication/RegisterForm.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRouter } from 'vue-router';
 import axios from 'axios';
 
+import { useUserStore } from '@/stores/user';
+
+const userStore = useUserStore()
+const router = useRouter()
 
 const form = ref({
     name: "",
@@ -22,6 +26,9 @@ async function register() {
         localStorage.setItem('access_token', response.data.data.access_token)
         localStorage.setItem('token_type', response.data.data.token_type)
         // localStorage.setItem('data', response.data)
+
+        userStore.fetchUser();
+        router.push('/')
     } catch (error) {
         console.error(error)
     }
--- src/components/layout/Logo.vue
+++ src/components/layout/Logo.vue
@@ -4,6 +4,6 @@ import { RouterLink } from 'vue-router'
 
 <template>
     <RouterLink to="/" class="flex items-center">
-                <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Zullkit Logo" />
-            </RouterLink>
+        <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Zullkit Logo" />
+    </RouterLink>
 </template>
